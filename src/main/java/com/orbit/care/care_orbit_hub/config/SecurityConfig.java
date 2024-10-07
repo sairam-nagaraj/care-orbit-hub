afe53647daf9cd6dca82baa55746a17c0bd8f178
Sairam N <sairam.nagaraj1992@gmail.com>
2024-10-08 04:19:55 +0530

    Added security to spring context

---
+++ src/main/java/com/orbit/care/care_orbit_hub/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package com.orbit.care.care_orbit_hub.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http)
+            throws Exception {
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer((resourceServer) -> resourceServer
+                        .jwt(Customizer.withDefaults()))
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+}
